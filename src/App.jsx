8e469872379b4f18b60b7e282360505191e7d4a7
Jawad El Yousfi <jel-yous@e3r2p8.1337.ma>
2025-01-13 18:32:04 +0100

    ll

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,14 @@ import Sop from './utils/stack'
 import { StyledCardRoot } from '@mui/joy/Card/Card'
 import { Button } from '@mui/joy'
 import solvePushSwap from './utils/solve'
+import Link from '@mui/joy/Link';
+import { IoIosBug } from "react-icons/io";
+import Divider from '@mui/joy/Divider';
+import Typography from '@mui/joy/Typography';
 
+
+
+import { IoLogoGithub } from "react-icons/io";
 
 
 function App() {
@@ -15,8 +22,8 @@ function App() {
   const stackA = useRef(null)
   const [stackADim, setStackADim] = useState({ width: 0, height: 0 })
   const [instructions, setInstructions] = useState([]);
-  const [elements, setElements] = useState([1, 2, 3, 4, 50, 6, 7, 8, 9, 10]);
-  const [indexedElements, setIndexedElements] = useState([{ num: 1, index: 0 }])
+  const [elements, setElements] = useState([]);
+  const [indexedElements, setIndexedElements] = useState([])
   const [b, setB] = useState([])
   const [styleE, setStyle] = useState('big bars + labels')
   const [classN, setClassN] = useState('stack-elements big-bars')
@@ -62,7 +69,16 @@ function App() {
 
 
   return (
-    <div className='flex h-screen lg:w-2/3 mx-auto '>
+    <div className='flex flex-col  gap-4'>
+      <div className='flex h-16 w-auto justify-center items-center gap-4' >
+      <Typography level="h2" sx={{ fontSize: 'xl', mb: 0.5 }}>
+    Push swap visualizer
+  </Typography>
+        <Button onClick={() => window.location = "https://github.com/jawadelyousfi17/push_swap_visualizer"} color='neutral' variant='outlined' startDecorator={<IoLogoGithub/> }>Github</Button>
+        <Button onClick={() => window.location = "https://github.com/jawadelyousfi17/push_swap_visualizer/issues"} color='neutral' variant='outlined' startDecorator={<IoIosBug/> }>Report bugs</Button>
+      <Divider/>
+      </div>
+       <div className='flex h-screen lg:w-2/3 mx-auto '>
       <div className='inst max-w-screen-sm'>
         <Instructions setShowindexes={setShowindexes} showIndxes={showIndxes}  setStyle={setStyle} ops={stackOps} setA={setElements} setB={setB} setInstructions={setInstructions}  instructions={instructions} ></Instructions>
       </div>
@@ -77,6 +93,9 @@ function App() {
       }} className={classN}>{styleE.includes('labels') && (showIndxes ? el.index : el.num)}   </div>)}
       </div>
     </div>
+      
+      </div>
+   
   )
 }
 
